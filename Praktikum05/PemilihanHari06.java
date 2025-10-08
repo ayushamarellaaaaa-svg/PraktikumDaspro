cb9c54b4f249e5f7ede5b3c1e7878df640a5e362
ayushamarellaaaaa-svg <ayushamarellaaaaa@gmail.com>
2025-10-08 10:47:44 +0700

    Modifikasi percobaan 2

--- Praktikum05/PemilihanHari06.java
+++ Praktikum05/PemilihanHari06.java
@@ -4,27 +4,39 @@ public class PemilihanHari06 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        String dayName, dayType;
+        System.out.println("Masukkan angka (1-7): ");
+        int angka = sc.nextInt();
 
-        System.out.println("Input day name: ");
-        dayName = sc.nextLine();
+        if (angka >= 1 && angka <= 5) {
+            System.out.println("Weekday");
+        } else if (angka == 6 || angka == 7) {
+            System.out.println("Weekend");
+        } else {
+            System.out.println("invalid Number");
 
-        switch (dayName.toLowerCase()) {
-            case "monday":
-            case "tuesday":
-            case "wednesday":
-            case "thursday":
-            case "friday":
-                dayType = "weekday";
-                break;
-            case "saturday":
-            case "sunday":
-                dayType = "weekend";
-                break;
-            default:
-                dayType = "invalid day name";
         }
 
-        System.out.println(dayName + " is a " + dayType);
+        // String dayName, dayType;
+
+        // System.out.println("Input day name: ");
+        // dayName = sc.nextLine();
+
+        // switch (dayName.toLowerCase()) {
+        //     case "monday":
+        //     case "tuesday":
+        //     case "wednesday":
+        //     case "thursday":
+        //     case "friday":
+        //         dayType = "weekday";
+        //         break;
+        //     case "saturday":
+        //     case "sunday":
+        //         dayType = "weekend";
+        //         break;
+        //     default:
+        //         dayType = "invalid day name";
+        // }
+
+        // System.out.println(dayName + " is a " + dayType);
     }
 }
