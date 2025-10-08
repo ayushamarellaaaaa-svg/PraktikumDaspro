c94522315fa9615f11cd8cc57afe9558c1e03567
ayushamarellaaaaa-svg <ayushamarellaaaaa@gmail.com>
2025-10-08 10:34:06 +0700

    Percobaan 1 ternary

--- Praktikum05/CetakKRS06.java
+++ Praktikum05/CetakKRS06.java
@@ -8,11 +8,17 @@ public class CetakKRS06 {
         System.out.println("Apakah UKT sudah lunas? (true/false)");
         boolean uktLunas = sc.nextBoolean();
 
-        if (uktLunas) {
-            System.out.println("Pembayaran UKT terverifikasi");
-            System.out.println("Silahkan cetak KRS dan minta tanda tangan DPA");
-        } else {
-            System.out.println("Registrasi ditolak. Silahkan lunasi UKT terlebih dahulu");
-        }
+        // if (uktLunas) {
+        //     System.out.println("Pembayaran UKT terverifikasi");
+        //     System.out.println("Silahkan cetak KRS dan minta tanda tangan DPA");
+        // } else {
+        //     System.out.println("Registrasi ditolak. Silahkan lunasi UKT terlebih dahulu");
+        // }
+
+        String pesan = (uktLunas)
+        ? "Pembayaran UKT terverifikasi. Silahkan cetak KRS dan minta tanda tangan DPA"
+        : "Registrasi ditolak. Silahkan lunasi UKT terlebih dahulu";
+
+        System.out.println(pesan);
     }
 }
