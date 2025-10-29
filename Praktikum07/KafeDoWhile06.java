0058f27d9b363cd448ce5bb0fe40212e2725a28b
ayushamarellaaaaa-svg <ayushamarellaaaaa@gmail.com>
2025-10-29 10:04:32 +0700

    percobaan 2

---
+++ Praktikum07/KafeDoWhile06.java
@@ -0,0 +1,16 @@
+package Praktikum07;
+
+import java.util.Scanner;
+
+public class KafeDoWhile06 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int nilai;
+        int jumlah;
+        int i = 0;
+
+        System.out.println("Masukkan jumlah mahasiswa: ");
+        jumlah = sc.nextInt();
+    }
+}
